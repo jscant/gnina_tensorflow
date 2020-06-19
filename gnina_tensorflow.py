934157235b263e7e43d24584877dbc68c8fc5144
jscant <jack.scantlebury@gmail.com>
2020-06-19 12:20:26 +0100

    Rewrote logic and documentation for process_batch();

--- gnina_tensorflow.py
+++ gnina_tensorflow.py
@@ -95,16 +95,45 @@ def get_test_info(test_file):
 
 def process_batch(model, example_provider, gmaker, input_tensor,
                   labels_tensor=None, train=True):
+    """
+    Feeds forward and backpropagates (if train==True) batch of examples.
+    
+    Arguments:
+        model: compiled tensorflow model
+        example_provider: molgrid.ExampleProvider object populated with a
+            types file
+        gmaker: molgrid.GridMaker object
+        input_tensor: molgrid.MGrid<x>f object, where <x> is the dimentions
+            of the input (including a dimention for batch size)
+        labels_tensor: molgrid.MGrid1f object, for storing true labels. If
+            labels_tensor is None and train is False, , return value will be
+            a vector of predictions.
+            
+    Returns:
+        if labels_tensor is None and train is False: numpy.ndarray of
+            predictions
+        if labels_tensor is specified and train is False: tuple containing
+            numpy.ndarray of labels and numpy.ndarray of predictions
+        if labels_tensor is specified and train is True: float containing
+            the mean loss over the batch (usually cross-entropy)
+            
+    Raises:
+        RuntimeError if labels_tensor is None and train is True
+    """
     if train and labels_tensor is None:
         raise RuntimeError('Labels must be provided for backpropagation',
                            'if train == True')
     batch = example_provider.next_batch(input_tensor.shape[0])
     gmaker.forward(batch, input_tensor, 0, random_rotation=True)
+    
+    if labels_tensor is None: # We don't know labels; just return predictions
+        return model.predict_on_batch(input_tensor.tonumpy())
+    
     batch.extract_label(0, labels_tensor) # y_true
-    if train:
+    if train: # Return loss
         return model.train_on_batch(
             input_tensor.tonumpy(), labels_tensor.tonumpy())
-    else:
+    else: # Return labels, predictions
         return (labels_tensor.tonumpy(),
                 model.predict_on_batch(input_tensor.tonumpy()))
 
