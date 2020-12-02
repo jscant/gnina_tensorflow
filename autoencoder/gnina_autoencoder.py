71203c118ab2fa218b10f829418229124a4ea577
Jack Scantlebury <jack.scantlebury@gmail.com>
2020-12-01 23:28:51 -0500

    Fixed typo.

--- autoencoder/gnina_autoencoder.py
+++ autoencoder/gnina_autoencoder.py
@@ -146,7 +146,7 @@ def main():
             metric_distance_threshold=args.metric_distance_threshold,
             learning_rate_schedule=lrs,
             adversarial=args.adversarial,
-            adversarial_variance=args.adverserial_variance,
+            adversarial_variance=args.adversarial_variance,
             **opt_args)
     else:
         ae.learning_rate_schedule = lrs
