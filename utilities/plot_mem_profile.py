2e8d3bd5bc740138a9aaec95da50d38c2f64d70b
Jack Scantlebury <jack.scantlebury@gmail.com>
2020-12-10 21:42:37 -0500

    Removed redundant mem_usage.py, made memory plotting prettier.

--- utilities/plot_mem_profile.py
+++ utilities/plot_mem_profile.py
@@ -15,6 +15,9 @@ try:
 except ImportError:
     upload_to_imgur = None
 
+USER = os.getenv('USER')
+HOSTNAME = socket.gethostname()
+
 
 def obtain_dataframe(raw_fnames, omit_fields=()):
     """Generate and sanitise dataframe given autoencoder working directories."""
@@ -23,9 +26,12 @@ def obtain_dataframe(raw_fnames, omit_fields=()):
         fname = Path(raw_fname).expanduser()
         for log in fname.rglob('**/memory_history.txt'):
             df = pd.read_csv(log, sep=' ')
-            df['Job'] = '/'.join(str(fname).split('/')[-4:])
+            df['Job'] = str(log.parent).replace(
+                str(Path('~').expanduser()), '~')
             res = pd.concat([res, df], copy=False)
     res['Usage'] /= 1024 ** 3
+    res = res[res['Time'] > 100]
+    res['Time'] -= 100
     for field in omit_fields:
         res = res[res['Memory_type'] != field]
     res.rename(columns={'Usage': 'Usage (GB)', 'Memory_type': 'Memory type',
@@ -38,13 +44,13 @@ def plot(df):
     g = sns.relplot(
         data=df,
         x='Time (s)', y='Usage (GB)',
-        hue='Memory type', row='Job',
+        hue='Memory type', col='Job',
         kind="line", legend='brief',
+        col_wrap=5,
         height=5, aspect=1, facet_kws=dict(
-            sharex=True, sharey=False, legend_out=True)
+            sharex=False, sharey=True, legend_out=True)
     )
     g.legend._visible = False
-    plt.legend()
     for ax in g.axes.ravel():
         ax.set_ylim([-0.02, 1.1 * ax.get_ylim()[1]])
         ax.set_xlim(left=0)
@@ -59,12 +65,15 @@ def plot(df):
             max_y, min_y = np.amax(y), np.amin(y)
             if max_y > highest_y:
                 highest_y = max_y
-                dy = max_y - min_y
+                dy = y[-1] - y[0]
                 dt = np.amax(x) - np.amin(x)
         dydt = 3600 * (dy / dt)
-        title += '\n({0}@{1})'.format(os.getenv('USER'), socket.gethostname())
         title += '\nChange per hour: {:.3f} GB'.format(dydt)
         ax.set_title(title)
+        ax.legend()
+    # g.fig.suptitle('({0}@{1})'.format(USER, HOSTNAME), fontsize=15)
+    # plt.legend()
+    plt.tight_layout()
     return g
 
 
